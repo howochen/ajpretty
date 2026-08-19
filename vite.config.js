ffe927edb129c9e36e0af5c34a5ee28983491a40
howogame <howochen@gmail.com>
2026-08-19 17:58:55 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // 若是 vue 請維持 vue 插件
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ajpretty/', // 加上這行（對應你的 Repo 名稱）
 })
